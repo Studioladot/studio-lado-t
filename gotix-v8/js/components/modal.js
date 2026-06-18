bc64d77d414e8920f65a31315d28e1ac34192f65
Gotix <kirizproject@gmail.com>
2026-06-18 18:21:35 -0300

    Delete gotix-v8/js directory

--- gotix-v8/js/components/modal.js
+++
@@ -1,8 +0,0 @@
-import { openModal as open, closeModal as close, openModalDynamic as dynamic } from '../utils/helpers.js';
-
-const modal = { open, close, dynamic };
-window.openModal = open;
-window.closeModal = close;
-window.openModalDynamic = dynamic;
-export default modal;
-export { open, close, dynamic };
